621f8afa6b38e0eb2fc31c7286105c6709b63f0c
EC2 Default User <ec2-user@ip-172-31-7-8.ap-northeast-1.compute.internal>
2021-03-26 14:02:38 +0000

    テスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+        flash[:notice] = "ログインに成功しました"
+        root_path
+  end
+
+  def after_sign_up_path_for(resource)
+        flash[:notice] = "新規登録が完了しました"
+        root_path
+  end
+
+
   protected
 
   def configure_permitted_parameters
